abe5e24802216185acfe0f1038187a62500b0ba1
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-08-01 18:51:21 +0200

    fase1

--- js/main.js
+++ js/main.js
@@ -19,3 +19,20 @@ const tasks = [
     },
 ];
 
+// Elemento del HTML donde queremos introducir la lista
+const list = document.querySelector('.list');
+
+// Mostrar una frase que muestre cuantas tareas hay
+/* a) Primero vamos a pintar una tarea, solo una, en una lista de HTML. A continuación vamos a preparar una clase que la modifique, de manera que si fuera una tarea completada completed: true, el texto aparezca tachado. */
+
+let res = `
+    <li class="item0">${tasks[0].name}</li>
+    `;
+
+list.innerHTML = res;
+
+const item0 = document.querySelector('.item0');
+
+if (tasks[0].completed === true) {
+    item0.classList.add('cross-out');
+}
